ba3a5428f140b846bbeab1b31e30e02b3772f87f
liquid54 <dk.sudent.course@gmail.com>
2025-02-19 17:04:51 +0200

    update subs

--- src/app/(admin)/subs/editSub.tsx
+++ src/app/(admin)/subs/editSub.tsx
@@ -5,46 +5,60 @@ import i18n from "@/app/i18n";
 const EditSub = () => {
     return (
         <div className="w-full flex items-center justify-center">
-            <div className="w-full max-w-md p-6 bg-white rounded-lg">
-                <div className="space-y-2">
-                    <div className="border-b border-[#D7DEDD] py-1" >
+            <div className="w-full max-w-[320px] sm:max-w-[400px] md:max-w-md
+                          p-4 sm:p-5 md:p-6
+                          bg-white rounded-lg">
+                <div className="space-y-4 sm:space-y-5 md:space-y-6">
+                    <div className="border-b border-[#D7DEDD]
+                                  pb-2 sm:pb-3 md:pb-4">
                         <ThemedText type="heading">
                             {i18n.t('adminPanelSubscriptions.titleEdit')}
                         </ThemedText>
                     </div>
 
-                    <div>
-                        <ThemedText type='subtext'>{i18n.t('adminPanelSubscriptions.name')}</ThemedText>
-                        <input
-                            type="text"
-                            className="w-full border-b border-black py-1"
-                            defaultValue="Тариф 'Місяць'"
-                        />
-                    </div>
-
-                    <div>
-                        <ThemedText type='subtext'>{i18n.t('adminPanelSubscriptions.description')}</ThemedText>
-                        <textarea
-                            className="w-full border-b border-black py-1 pt-[32px]"
-                            defaultValue="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-                        />
-                    </div>
-
-                    <div>
-                        <ThemedText type='subtext'>{i18n.t('adminPanelSubscriptions.price')}</ThemedText>
-                        <div className="flex items-center">
+                    <div className="space-y-2 sm:space-y-3 md:space-y-4">
+                        <div>
+                            <ThemedText type='subtext'>
+                                {i18n.t('adminPanelSubscriptions.name')}
+                            </ThemedText>
                             <input
                                 type="text"
-                                className="w-full border-b border-black py-1"
-                                defaultValue="14"
+                                className="w-full border-b border-black py-1 sm:py-2 outline-none"
+                                defaultValue="Тариф 'Місяць'"
                             />
+                        </div>
+
+                        <div>
+                            <ThemedText type='subtext'>
+                                {i18n.t('adminPanelSubscriptions.description')}
+                            </ThemedText>
+                            <textarea
+                                className="w-full border-b border-black
+                                         py-1 sm:py-2
+                                         pt-4 sm:pt-6 md:pt-[32px]
+                                         min-h-[80px] outline-none resize-none"
+                                defaultValue="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                            />
+                        </div>
+
+                        <div>
+                            <ThemedText type='subtext'>
+                                {i18n.t('adminPanelSubscriptions.price')}
+                            </ThemedText>
+                            <div className="flex items-center">
+                                <input
+                                    type="text"
+                                    className="w-full border-b border-black py-1 sm:py-2 outline-none"
+                                    defaultValue="14"
+                                />
+                            </div>
                         </div>
                     </div>
 
-                    <div className="pt-4">
+                    <div className="pt-4 sm:pt-6 md:pt-8">
                         <Button
                             variant="primary"
-                            className="w-full bg-black text-white py-3"
+                            className="w-full bg-black text-white py-2 sm:py-3"
                         >
                             {i18n.t('adminPanelSubscriptions.save')}
                         </Button>
--- src/app/(admin)/subs/subsPage.tsx
+++ src/app/(admin)/subs/subsPage.tsx
@@ -5,33 +5,25 @@ import {ThemedText} from "@/app/components/ThemedText";
 import EditSub from "@/app/(admin)/subs/editSub";
 import i18n from "@/app/i18n";
 
-
-
-
 const SubsPage = () => {
-
-
-
-
-
     return (
         <SplitLayout className="flex h-[100wh]">
             <div>
-                <div className="flex justify-center pt-[26px] pb-[43px]" >
+                <div className="flex justify-center pt-4 sm:pt-6 md:pt-[26px] pb-6 sm:pb-8 md:pb-[43px]">
                     <ThemedText type='heading'>{i18n.t('adminPanelSubscriptions.titleSubscriptions')}</ThemedText>
                 </div>
                 <Plans plans={pricingPlans} />
             </div>
 
-
             <div>
-                <div className='px-[220px] pt-[151px] content-center'>
+                <div className="px-4 sm:px-6 md:px-8 lg:px-[220px]
+                               pt-6 sm:pt-8 md:pt-12 lg:pt-[151px]
+                               content-center">
                     <EditSub/>
                 </div>
             </div>
         </SplitLayout>
     );
 }
-
 
 export default SubsPage;
